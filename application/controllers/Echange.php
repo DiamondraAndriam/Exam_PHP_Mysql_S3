a6d82f761ed637e0f19e8c3e3ce4607fe70f522b
DiamondraAndriam <diamondra.andriamampiandra@gmail.com>
2023-02-08 10:56:30 +0300

    debut echange

--- application/controllers/Echange.php
+++ application/controllers/Echange.php
@@ -2,6 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Echange extends CI_Controller {
-
+    public function echanger($id_Objet){
+        if($id_Objet==null){
+            redirect("/");
+        }
+        /*
+        $this -> load -> model("_model");
+        $data['mesObjets'] = $this -> _model -> findMyObjects;
+        
+        */
+        $data['id_obj'] = $id_Objet;
+        $this->load->view('echange', $data);
+    }
 }
 ?>
