5a7c6bbb3267d227b3fd227c153ba3aa0f22fb7b
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2023-02-08 06:34:52 +0300

    update login

--- application/views/inscription.php
+++ application/views/inscription.php
@@ -7,21 +7,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="asset/style_login.css">
+    <link rel="stylesheet" type="text/css" href="assets/style_log.css">
     <title>Login</title>
 </head>
 <body>
+    <a href="user"><div id="login_link">Se connecter</div></a>
     <div id="form">
-    <h1>S'inscrire</h1>
+    <h2>S'inscrire</h2>
     <form action ="treatInscription" method="post">
         <div class="legend">Nom</div>
-        <div class="input"><input type="text" name="nom"></div>
+        <div class="input"><input type="text" name="nom" placeholder="Nom"></div>
         <div class="legend">Prénoms</div>
-        <div class="input"><input type="text" name="prenoms"></div>
+        <div class="input"><input type="text" name="prenoms" placeholder="Prénoms"></div>
         <div class="legend">E-mail</div>
-        <div class="input"><input type="email" name="email"></div>
+        <div class="input"><input type="email" name="email" placeholder="Email"></div>
         <div class="legend">Mot de passe</div>
-        <div class="input"><input type="password" name="mdp"></div>
+        <div class="input"><input type="password" name="mdp" placeholder="Mot de passe"></div>
         <div><input type="submit" value="S'inscrire"></div>
     </form>
     </div>
--- application/views/login_user.php
+++ application/views/login_user.php
@@ -11,13 +11,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>Login</title>
 </head>
 <body>
+    <a href="user"><div id="login_link">Se connecter</div></a>
     <div id="form">
     <h1>Se connecter</h1>
     <form action ="treatLogUser" method="post">
         <div class="legend">E-mail</div>
-        <div class="input"><input type="email" name="email"></div>
+        <div class="input"><input type="email" name="email" placeholder="E-mail"></div>
         <div class="legend">Mot de passe</div>
-        <div class="input"><input type="password" name="pwd"></div>
+        <div class="input"><input type="password" name="pwd" placeholder="Mot de passe"></div>
         <div><input type="submit" value="Se connecter"></div>
     </form>
         <hr>
